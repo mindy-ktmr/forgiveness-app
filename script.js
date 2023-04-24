dfa293fade3ccc980830ccfff1e573456fd3b322
mindy-ktmr <131720605+mindy-ktmr@users.noreply.github.com>
2023-04-24 18:14:37 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const appDiv = document.getElementById("app");
+  appDiv.innerHTML = "<p>Hello, world! This is where your app will be built.</p>";
+});
