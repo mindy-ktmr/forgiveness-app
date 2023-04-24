d5a83e5b3ea9ec19e7bfaa23c8a8e6fc6869888a
mindy-ktmr <131720605+mindy-ktmr@users.noreply.github.com>
2023-04-24 18:36:30 -0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,13 +3,28 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/generate-character', methods=['POST'])
+import openai
+
 def generate_character():
     preferences = request.json['preferences']
-    # Here is where you'll add the code to generate a character using GPT
+    
+    prompt = f"Generate an unpleasant character based on the following preferences: {preferences}"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    character_text = response.choices[0].text.strip()
+
     character = {
-        'name': 'John Doe'
+        'text': character_text
     }
     return jsonify(character)
+
 
 if __name__ == '__main__':
     app.run()
