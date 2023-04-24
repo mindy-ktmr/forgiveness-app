1628ff4fcc22e47224943e7a3aad86031040cee1
mindy-ktmr <131720605+mindy-ktmr@users.noreply.github.com>
2023-04-24 18:17:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const appDiv = document.getElementById("app");
-  appDiv.innerHTML = "<p>Hello, world! This is where your app will be built.</p>";
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("preferences-form");
+  const characterContainer = document.getElementById("character-container");
+  const characterDiv = document.getElementById("character");
+  const forgivenessSteps = document.getElementById("forgiveness-steps");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const preferences = document.getElementById("preferences").value;
+    const generatedCharacter = generateCharacter(preferences);
+
+    characterDiv.innerHTML = `<p>Name: ${generatedCharacter.name}</p>`;
+    characterContainer.style.display = "block";
+
+    displayForgivenessSteps();
+  });
 });
+
+function generateCharacter(preferences) {
+  // You can update this function to generate a character based on the user's preferences
+  return {
+    name: "John Doe",
+  };
+}
+
+function displayForgivenessSteps() {
+  // You can update this function to display the forgiveness steps
+  const steps = `
+    <h3>Step 1: Rate your reaction</h3>
+    <p>...</p>
+    <h3>Step 2: Perspective shift</h3>
+    <p>...</p>
+    <h3>Step 3: Visualize forgiveness</h3>
+    <p>...</p>
+  `;
+
+  forgivenessSteps.innerHTML = steps;
+}
