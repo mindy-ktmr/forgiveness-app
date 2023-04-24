735b7c303062e7e11f5350c3e0dab9baeae82728
mindy-ktmr <131720605+mindy-ktmr@users.noreply.github.com>
2023-04-24 18:27:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,22 +17,33 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function generateCharacter(preferences) {
-  // You can update this function to generate a character based on the user's preferences
+  const names = ["John Doe", "Jane Smith", "Sam Brown", "Olivia Johnson"];
+  const selectedIndex = Math.floor(Math.random() * names.length);
   return {
-    name: "John Doe",
+    name: names[selectedIndex],
   };
 }
 
+
 function displayForgivenessSteps() {
-  // You can update this function to display the forgiveness steps
   const steps = `
     <h3>Step 1: Rate your reaction</h3>
-    <p>...</p>
+    <p>On a scale of 1 to 10, rate the intensity of your negative emotions towards the character.</p>
+    <input type="number" id="reaction-rating" min="1" max="10" value="1">
     <h3>Step 2: Perspective shift</h3>
-    <p>...</p>
+    <p>Take a moment to consider the character's situation and background. Try to understand why they might act the way they do.</p>
     <h3>Step 3: Visualize forgiveness</h3>
-    <p>...</p>
+    <p>Imagine letting go of your negative emotions towards the character. Visualize yourself forgiving them and moving forward.</p>
+    <button id="forgive-button">I forgive this character</button>
   `;
 
   forgivenessSteps.innerHTML = steps;
+
+  const forgiveButton = document.getElementById("forgive-button");
+  forgiveButton.addEventListener("click", function () {
+    alert("You've forgiven the character!");
+    characterContainer.style.display = "none";
+    forgivenessSteps.innerHTML = "";
+    document.getElementById("preferences").value = "";
+  });
 }
